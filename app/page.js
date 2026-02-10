5960a6a3a37914f0d762278c1e355663637f5876
tanoshuyanos <39433379+tanoshuyanos@users.noreply.github.com>
2026-02-10 09:39:04 +0500

    Update app version from v7.0 to v7.1

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ import {
   Package, Scale, ShieldCheck, Keyboard, History, GitMerge, Settings, ChevronRight
 } from 'lucide-react';
 
-const APP_VERSION = "v7.0 (Stable Monolith + Routes)"; 
+const APP_VERSION = "v7.1 (Stable Monolith + Routes)"; 
 const STAND_URL = "https://script.google.com/macros/s/AKfycbwPVrrM4BuRPhbJXyFCmMY88QHQaI12Pbhj9Db9Ru0ke5a3blJV8luSONKao-DD6SNN/exec"; 
 const SHEET_URL = "https://docs.google.com/spreadsheets/d/1Bf...ВАША_ССЫЛКА.../edit"; 
 
