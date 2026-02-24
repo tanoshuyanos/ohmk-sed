35ca4fe8f70888b89368a31c8d0f02d37b9c450a
tanoshuyanos <39433379+tanoshuyanos@users.noreply.github.com>
2026-02-24 13:04:01 +0500

    Update app version to v11.04

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ import {
   Monitor
 } from 'lucide-react';
 
-const APP_VERSION = "v11.03 (Dir+AI+Table)";
+const APP_VERSION = "v11.04 (Dir+AI+Table)";
 // Вставь свои ссылки:
 const STAND_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwKPGj8wyddHpkZmbZl5PSAmAklqUoL5lcT26c7_iGOnFEVY97fhO_RmFP8vxxE3QMp/exec"; // ССЫЛКА НА ТАБЛО
 const STAND_URL = "https://script.google.com/macros/s/AKfycbwPVrrM4BuRPhbJXyFCmMY88QHQaI12Pbhj9Db9Ru0ke5a3blJV8luSONKao-DD6SNN/exec"; 
@@ -184,7 +184,7 @@ export default function SED() {
       if (actionType === 'TOGGLE_URGENCY') {
           updates.urgency = payload.isUrgent ? null : 'срочно';
       }
-    if (role === 'DIRECTOR') {
+      else if (role === 'DIRECTOR') {
           if (actionType === 'APPROVE') updates.step_director = 1;
           if (actionType === 'REJECT') updates.step_director = 0;
       }
